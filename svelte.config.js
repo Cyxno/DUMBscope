f980db927adcb92e4730c837854a607448d97b2a
Cyxno <Cyxno@users.noreply.github.com>
2026-09-10 17:14:31 +0000

    chore: release v0.2.0

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,7 @@ const config = {
 	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter(),
-		version: { name: process.env.npm_package_version ?? '0.1.1' },
+		version: { name: process.env.npm_package_version ?? '0.2.0' },
 		csp: {
 			mode: 'hash',
 			directives: {
